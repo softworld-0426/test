e2ff8bcc696558cb294b22a5b150f77edf5eb138
gustavocastro <gustavoprasca@hotmail.com>
2022-06-14 10:43:01 -0500

    init test

--- src/main/java/com/co/softworld/NameApplication.java
+++ src/main/java/com/co/softworld/NameApplication.java
@@ -1,13 +1,20 @@
 package com.co.softworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NameApplication {
+public class NameApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NameApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String java_Variable = "hi";
+		String GT = "test";
+		System.out.println("This is a test");
+	}
 }
